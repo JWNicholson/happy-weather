85d5514f48648e5b8c39e435ca0f0623c75e19f8
JW Nicholson <john.nicholson812@gmail.com>
2020-09-11 09:14:12 -0400

    updated App & index css files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import './App.module.css';
+import styles from './App.module.css';
+import {
+  Container, Grid
+}from '@material-ui/core';
 
-import Clock from './components/Clock/Clock'
+
 import GetWeather from './components/GetWeather/GetWeather';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    <main className="content-wrapper">
+    <>
+    <Header />
+    <Container maxWidth="md" className={styles.App}>
+      
     {/* user search input  */}
 
-      {/* render time 7 date */}
-      <Clock />
 
       {/* render weather conditions */}
       <GetWeather />
      
-      </main>
+      </Container>
+      
      <Footer />
-    </div>
+     </>
+    
   );
 }
 
 export default App;
+
+
